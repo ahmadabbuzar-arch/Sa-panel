c2ce161997468385191f3697d42189d2db677cf0
ahmadabbuzar-arch <ahmadabbuzar@gmail.com>
2026-05-13 21:32:48 +0530

    Update services.js

--- api/services.js
+++ api/services.js
@@ -3,10 +3,10 @@ export default async function handler(req, res) {
   
   try {
     const params = new URLSearchParams();
-    params.append('key', '4b70fbf9cddd361279924a37086961d1');
+    params.append('key', 'e6428cab83136a49fae4a92a9aa4b1cc');
     params.append('action', 'services');
 
-    const response = await fetch('https://smmsbi.com/api/v2', {
+    const response = await fetch('https://smmfollows.com/api/v2', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: params.toString()
