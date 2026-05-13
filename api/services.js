28100b973e9addc0ae4cfa86de45d246d5541de3
ahmadabbuzar-arch <ahmadabbuzar@gmail.com>
2026-05-14 00:44:44 +0530

    Update services.js

--- api/services.js
+++ api/services.js
@@ -1,17 +1,14 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  
   try {
     const params = new URLSearchParams();
-    params.append('key', 'e6428cab83136a49fae4a92a9aa4b1cc');
+    params.append('key', '4c7b73bd82d0c2dc0226d6cf2121bb01');
     params.append('action', 'services');
-
-    const response = await fetch('https://smmfollows.com/api/v2', {
+    const response = await fetch('https://smmwiz.com/api/v2', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      headers: {'Content-Type':'application/x-www-form-urlencoded'},
       body: params.toString()
     });
-
     const data = await response.json();
     res.status(200).json(data);
   } catch(e) {
