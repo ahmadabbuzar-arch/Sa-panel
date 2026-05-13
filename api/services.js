a89ca6f11742e69f1828b88f3fa7715a03ec6fd1
ahmadabbuzar-arch <ahmadabbuzar@gmail.com>
2026-05-13 17:41:36 +0530

    Add files via upload

---
+++ api/services.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  try {
+    const params = new URLSearchParams();
+    params.append('key', '4b70fbf9cddd361279924a37086961d1');
+    params.append('action', 'services');
+
+    const response = await fetch('https://smmsbi.com/api/v2', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params.toString()
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch(e) {
+    res.status(500).json({error: e.message});
+  }
+}
